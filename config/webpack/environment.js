1e1f7e22d77dbd2793a9ebd3c5573f29fd266fca
Daniel Alves <daniel@danielalves.me>
2018-12-30 01:21:21 +0100

    Update vue-loader

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,12 @@
 const { environment } = require('@rails/webpacker')
 const vue =  require('./loaders/vue')
 
+// Required for vue-loader v15
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+environment.plugins.append(
+  'VueLoaderPlugin',
+  new VueLoaderPlugin()
+)
+
 environment.loaders.append('vue', vue)
 module.exports = environment
